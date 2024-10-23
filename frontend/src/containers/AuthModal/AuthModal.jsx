4b514e908f5c9cb38602330c31cad46a96c4d8f0
Tamara Vdb <vdb.tamara@gmail.com>
2024-10-23 15:29:01 +0200

    Update Design Modal 🧼

--- frontend/src/containers/AuthModal/AuthModal.jsx
+++ frontend/src/containers/AuthModal/AuthModal.jsx
@@ -7,7 +7,7 @@ const AuthModal = ({ visible, modalType, onClose, switchToLogin, switchToSignup 
    return (
       <div className="fixed top-0 left-0 w-full h-full z-50 bg-gray-700/50">
          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-md w-[90%] sm:w-auto mx-auto bg-white">
-            <button onClick={onClose} className="absolute top-4 right-4 font-semibold rounded-full bg-custom-blue text-white w-7 h-7">X</button>
+            <button onClick={onClose} className="absolute top-4 right-4 font-semibold rounded-full bg-text text-white w-7 h-7">X</button>
 
             {/* Affiche Login ou Signup en fonction de modalType */}
             {modalType === 'login' ? (
