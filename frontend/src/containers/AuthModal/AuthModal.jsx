eedd8723e2c388c106128fe4212bebf51e528b99
Amandine Ameye <amandineameye@gmail.com>
2024-10-24 01:11:52 +0200

    Gestion des événement du login et du signup (y compris POST requests avec URL ficitif) et ajout du texte d’accueil

--- frontend/src/containers/AuthModal/AuthModal.jsx
+++ frontend/src/containers/AuthModal/AuthModal.jsx
@@ -1,23 +1,37 @@
-import Login from '../../components/Login/Login';
-import Signup from '../../components/Signup/Signup';
+import Login from "../../components/Login/Login";
+import Signup from "../../components/Signup/Signup";
 
-const AuthModal = ({ visible, modalType, onClose, switchToLogin, switchToSignup }) => {
-   if (!visible) return null; // Modal is not visible
+const AuthModal = ({
+	visible,
+	modalType,
+	onClose,
+	switchToLogin,
+	switchToSignup,
+}) => {
+	if (!visible) return null; // Modal is not visible
 
-   return (
-      <div className="fixed top-0 left-0 w-full h-full z-50 bg-gray-700/50">
-         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-md w-[90%] sm:w-auto mx-auto bg-white min-w-[400px]">
-            <button onClick={onClose} className="absolute top-4 right-4 font-semibold rounded-full bg-text text-white w-7 h-7">X</button>
+	return (
+		<div className="fixed top-0 left-0 w-full h-full z-50 bg-gray-700/50">
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl shadow-md w-[90%] sm:w-auto mx-auto bg-white min-w-[400px]">
+				<button
+					onClick={onClose}
+					className="absolute top-4 right-4 font-semibold rounded-full bg-text text-white w-7 h-7"
+				>
+					X
+				</button>
 
-            {/* Affiche Login ou Signup en fonction de modalType */}
-            {modalType === 'login' ? (
-               <Login onSwitchToSignup={switchToSignup} />
-            ) : (
-               <Signup onSwitchToLogin={switchToLogin} />
-            )}
-         </div>
-      </div>
-   );
+				{/* Affiche Login ou Signup en fonction de modalType */}
+				{modalType === "login" ? (
+					<Login onSwitchToSignup={switchToSignup} />
+				) : (
+					<Signup
+						onSwitchToLogin={switchToLogin}
+						onSuccessfulConnection={onClose}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default AuthModal;
--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,45 +1,67 @@
-import homeImage from '../../assets/home.png';
+import homeImage from "../../assets/home.png";
 
 const HomePage = () => {
+	return (
+		<>
+			<main className="flex flex-1 py-4">
+				<div className="container flex flex-col xl:flex-row gap-4">
+					<div className="flex flex-col  w-full xl:w-1/2 bg-[#f5f8f6] rounded-lg justify-center py-10 px-10">
+						<h2>Carbon Quest, quésaco ?</h2>
+						<div>
+							<p className="pb-4">
+								Carbon Quest est un jeu éducatif de plates-formes qui vous
+								invite à explorer les enjeux du Green IT. Le Green IT désigne
+								l’ensemble des pratiques visant à réduire l’impact
+								environnemental des technologies numériques, de leur conception
+								à leur utilisation.
+							</p>
+							<p className="pb-4">
+								Dans ce jeu, vous incarnez un avatar qui grimpe dans une forêt,
+								répondant à des questions sur ces pratiques écologiques. À la
+								fin de l'ascension, vous découvrirez un ciel reflétant votre
+								succès : sera-t-il radieux ou couvert de pollution numérique ?
+							</p>
+							<h3>But du jeu</h3>
+							<p className="pb-4">
+								Votre mission est de grimper jusqu’au sommet de la canopée avant
+								la fin du temps imparti, tout en répondant correctement à un
+								maximum de questions. Votre score final influence l’état du
+								ciel, que vous ne découvrirez qu’une fois arrivés en haut. Un
+								ciel dégagé est le signe de bonnes réponses, tandis qu’un ciel
+								sombre reflète des erreurs accumulées.
+							</p>
 
-   return (
-      <>
-         <main className="flex flex-1 py-4">
-            <div className="container flex flex-col xl:flex-row gap-4">
-               <div className="flex flex-col  w-full xl:w-1/2 bg-[#f5f8f6] rounded-lg justify-center py-10 px-10">
-                  <h2>Carbon Quest, Quésako ?</h2>
-                  <div>
-                     <p className="pb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a nisl est. Curabitur pellentesque in lectus eget aliquam. Aenean ante ex, hendrerit vel tortor ac, tempor mollis metus. Morbi luctus et felis nec lacinia.</p>
-                     <h3>But du jeux</h3>
-                     <p className="pb-4">
-                        Integer ac lacinia eros, sit amet mollis erat. Sed eget libero eget quam ultrices mollis sit amet ut arcu. Nunc ut urna fringilla sapien fermentum finibus. Pellentesque vehicula pretium vulputate.
-                     </p>
-                     <h3>Comment jouer ?</h3>
-                     <ul className="pb-4">
-                        <li>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </li>
-                        <li>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </li>
-                        <li>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </li>
-                        <li>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </li>
-                     </ul>
-                     <p className="pb-4">Arriveras-tu à ...</p>
-                     <button className="flex text-md font-semibold bg-title hover:bg-text-light transition-200 rounded-lg text-white px-5 py-2 transition-200 mx-auto mt-8">Start Playing</button>
-                  </div>
-               </div>
-               <div className="flex flex-col w-full xl:w-1/2">
-                  <img src={homeImage} alt="" />
-               </div>
-            </div>
-         </main>
-      </>
-   );
+							<h3>Comment jouer ?</h3>
+							<ul className="pb-4">
+								<li>
+									Dans Carbon Quest, utilisez les touches directionnelles pour
+									déplacer votre avatar vers le haut de la forêt.
+								</li>
+								<li>
+									À chaque palier, un esprit de la forêt vous posera une
+									question sur le Green IT.
+								</li>
+								<li>
+									Une mauvaise décision ne vous empêche pas de continuer à
+									monter, mais elle affectera le résultat final.
+								</li>
+								<li>
+									Une fois au sommet, vous pourrez consulter vos réponses, voir
+									vos erreurs, et en apprendre davantage sur le Green IT.
+								</li>
+							</ul>
+							<button className="flex text-md font-semibold bg-title hover:bg-text-light transition-200 rounded-lg text-white px-5 py-2 transition-200 mx-auto mt-8">
+								Jouer
+							</button>
+						</div>
+					</div>
+					<div className="flex flex-col w-full xl:w-1/2">
+						<img src={homeImage} alt="" />
+					</div>
+				</div>
+			</main>
+		</>
+	);
 };
 
 export default HomePage;
