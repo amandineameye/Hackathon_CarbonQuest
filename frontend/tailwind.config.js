6523a34b58a28dc84dc71f53622e106ea6305a19
Tamara Vdb <vdb.tamara@gmail.com>
2024-10-23 11:26:48 +0200

    Config Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {
+   content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+   ],
+   theme: {
+      fontFamily: {
+         'title': ['Josefin', 'sans-serif']
+      },
+
+      extend: {
+         fontFamily: {
+            'sans': ['Quicksand', ...defaultTheme.fontFamily.sans],
+         },
+         colors: {
+            ...colors,
+            'text': '#12a495b',
+            'title': '#1c2743',
+            'custom-green': '#6b917e',
+            'custom-light-green': '#a6c998',
+            'custom-dark-green': '#66856c',
+            'custom-yellow': '#c8d87d',
+            'custom-purple': '#b19b95',
+            'custom-pink': '#d6827c',
+            'custom-grey': '#849a98',
+         },
+         container: {
+            center: true,
+            padding: {
+               DEFAULT: '1rem',
+               md: '2rem',
+               lg: '3rem',
+            },
+         },
+      },
+   },
+   plugins: [],
+};
 
