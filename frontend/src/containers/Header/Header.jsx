aac67a37a1670fe7dbf0061761a007faa6222490
Tamara Vdb <vdb.tamara@gmail.com>
2024-10-24 12:01:17 +0200

    Add Link to Title

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -1,17 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
-   
 
    return (
       <>
          <header className="shadow-lg top-0 left-0 w-full z-10 bg-custom-green">
             <div className="container py-4 flex justify-between items-center">
-               <div className="font-logo text-custom-blue text-2xl sm:text-3xl">
-                  Carbon Quest
-               </div>
-              
+               <Link to="/">
+                  <div className="font-logo text-custom-blue text-2xl sm:text-3xl">Carbon Quest</div>
+               </Link>
             </div>
          </header>
-         
       </>
    );
 };
