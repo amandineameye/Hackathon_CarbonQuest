0aa7b9d08598a62bb1f5154030dc28e996a50e3e
Tamara Vdb <vdb.tamara@gmail.com>
2024-10-24 12:06:50 +0200

    Merge

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -2,9 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 const Header = () => {
 
-
    const navigate = useNavigate();
-
 
    const handleClick = () => {
       navigate("/");
@@ -14,7 +12,7 @@ const Header = () => {
       <>
          <header className="shadow-lg top-0 left-0 w-full z-10 bg-custom-green">
             <div className="container py-4 flex justify-between items-center">
-               <div className="font-logo text-custom-blue text-2xl sm:text-3xl">
+               <div onClick={handleClick} className="font-logo text-custom-blue text-2xl sm:text-3xl">
                   Carbon Quest
                </div>
 
