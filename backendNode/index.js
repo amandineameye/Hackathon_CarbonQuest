82d2b655c0eee41eeb0df6a230b4f4bd90b3112d
Amandine Ameye <amandineameye@gmail.com>
2024-12-27 21:09:29 +0100

    Test

--- backendNode/index.js
+++ backendNode/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+// require("dotenv").config();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
